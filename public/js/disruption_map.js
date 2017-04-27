7c2eeeca7f43aa99c1b6324ded636784316e25af
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-27 18:51:12 +0100

    Add traffic disruptions onto the map, clustered with google markerclusterer

--- public/js/disruption_map.js
+++ public/js/disruption_map.js
@@ -1,7 +1,19 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.5060624, lng: -0.1332773},
-    zoom: 10
+  $.get('/disruptions', function(disruptions) {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 51.5060624, lng: -0.1332773 },
+      zoom: 10
+    });
+    var markers = [];
+
+    disruptions.map(function(disruption) {
+      disruption.display_points.map(function(latlng) {
+        markers.push(new google.maps.Marker({position: latlng}));
+      });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers,
+    { imagePath: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/master/markerclustererplus/images/m' });
   });
 }
