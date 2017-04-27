a921907e0b48ef0f0c134c298e53af247018ea9c
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-27 22:36:29 +0100

    Add started and ended date to info window on pins

--- public/js/disruption_map.js
+++ public/js/disruption_map.js
@@ -6,7 +6,7 @@ function initMap() {
     'Scheduled': 'green',
     'Recurring Works': 'brown',
     'Recently Cleared': 'green'
-  }
+  };
 
   $.get('/disruptions', function(disruptions) {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -16,10 +16,8 @@ function initMap() {
     var markers = [];
 
     disruptions.map(function(disruption) {
-      var statusText = '<h3><b>' + disruption.status + '</b></h3>'
-      var content = statusText + '<p>' + disruption.commentary + '</p>'
-      var infoWindow = new google.maps.InfoWindow({ content: content })
       disruption.display_points.map(function(latlng) {
+        var infoWindow = createInfoWindow(disruption);
         var icon = 'images/markers/' + STATUS_COLOURS[disruption.status] + '_MarkerA.png'
         var marker = new google.maps.Marker({ position: latlng, icon: icon })
         marker.addListener('click', function() {
@@ -32,4 +30,13 @@ function initMap() {
     var markerCluster = new MarkerClusterer(map, markers,
     { maxZoom: 15, gridSize: 50, imagePath: 'https://raw.githubusercontent.com/googlemaps/v3-utility-library/master/markerclustererplus/images/m' });
   });
+
+  function createInfoWindow(disruption) {
+    // Set up infoWindow on each pin
+    var statusText = '<h3><b>' + disruption.status + '</b></h3>';
+    var startTimeText = '<p><b>Started:</b> ' + disruption.start_time + '</p>';
+    var endTimeText = disruption.end_time ? '<p><b>Enjing:</b> ' + disruption.end_time + '</p>' : "";
+    var content = statusText + startTimeText + endTimeText + '<p>' + disruption.commentary + '</p>';
+    return new google.maps.InfoWindow({ content: content });
+  }
 }
