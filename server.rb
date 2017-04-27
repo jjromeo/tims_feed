881114289bbea5a77dd539c6b864c44a402053d7
Jerome Pratt <jerome.pratt@outlook.com>
2017-04-27 18:33:02 +0100

    Add an endpoint to get display points from tfl

--- server.rb
+++ server.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require './lib/tims_feed'
 
 get '/' do
   erb :index
+end
+
+get '/disruptions' do
+  information_points = TimsFeed.new.information_points
+  content_type 'application/json'
+  information_points.to_json
 end
---
+++ spec/home_page_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../server.rb'
+require 'rack/test'
+
+RSpec.describe 'server' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it 'displays homepage' do
+    get '/'
+    expect(last_response.status).to eq(200)
+  end
+
+  context 'rendering json of disruptions' do
+
+    def get_first_disruption
+      VCR.use_cassette('disruptions') do
+        get '/disruptions'
+        expect(last_response.body).to be_a(String)
+        JSON.parse(last_response.body).first
+      end
+    end
+
+    before(:context) do
+      @first_disruption = get_first_disruption
+    end
+
+    it 'gets a commentary of disruption' do
+      expect(@first_disruption["commentary"]).to eq "[A3211] Upper Thames Street (EC4R/EC4V) (Westbound) between Cousin Lane and Queen Street Place - Lane one (of two) closed to facilitate high voltage repair works. "
+    end
+
+    it 'gets an array of coordinates to display points' do
+      expect(@first_disruption["display_points"].first).to eq({ "longitude" => "532469", "latitude" => "180821" })
+    end
+  end
+end
